c2a220d573d20db1eab6e61bcd3ee471de7fa061
Dylan Maxwell <maxwelld@frib.msu.edu>
2013-10-02 12:16:20 -0400

    Rewrite to single build script instead of building 'core' and 'applications' separately.

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+BUILDER=`dirname "$0"`
+. "$BUILDER/util/build-functions.sh"
+
+
+clear_build_directory
+
+clear_build_repository
+
+
+cp -f $BUILDER/maps/*.map "$MAPS_DIRECTORY"
+
+
+build_feature "org.csstudio.build.feature"
+if [ $? != 0 ]; then
+	echo "Error building: org.csstudio.core.build.feature"
+	exit 1
+fi
+
+# generate_map_p2 "cs-studio.p2.map"
